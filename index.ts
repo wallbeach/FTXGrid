e2210f251ecab58cca133d408e5022c89a7b9e6d
wallbeach <thierry.thomann@gmail.com>
2021-12-14 07:40:09 +0100

    Update index.ts
    Better error handling

--- index.ts
+++ index.ts
@@ -7,15 +7,15 @@ import { Portfolio } from "./Portfolio.js";
 
 const config = new Config();
 
-const key             = config.key;
-const secret          = config.secret;
-const subAccountName  = config.subAccountName;
-const market          = config.market;
+const key = config.key;
+const secret = config.secret;
+const subAccountName = config.subAccountName;
+const market = config.market;
 
-const _stepDistance   = config.stepDistance;
-const _usedEquity     = config.usedEquity;
-const _minEquity      = config.minEquity;
-const _takeProfit     = config.takeProfit;
+const _stepDistance = config.stepDistance;
+const _usedEquity = config.usedEquity;
+const _minEquity = config.minEquity;
+const _takeProfit = config.takeProfit;
 
 const restClientOptions = { subAccountName: subAccountName };
 
@@ -51,11 +51,8 @@ async function start() {
   // console.log(`[INFO] Total Balance BTC: ${totalBalanceBTC} - Free Balance BTC: ${freeBalanceBTC}`);
   // console.log(`[INFO] Total Balance ETH: ${totalBalanceETH} - Free Balance ETH: ${freeBalanceETH}`);
 
-
   // 3. Get current market prices
   let _marketBTC = await client.getMarket(market).catch(console.error);
-
-
 
   lastPrice = _marketBTC.result.last;
   currBid = _marketBTC.result.bid;
@@ -67,7 +64,14 @@ async function start() {
   const utcDate = new Date(Date.now());
 
   // Store Portfolio value
-  let portfolio: IPortfolio = {market: market, timestamp: Date.now().toLocaleString(), btc: totalBalanceBTC, eth: totalBalanceETH, curr_bid: currBid, total_btc: totalinbtc};
+  let portfolio: IPortfolio = {
+    market: market,
+    timestamp: Date.now().toLocaleString(),
+    btc: totalBalanceBTC,
+    eth: totalBalanceETH,
+    curr_bid: currBid,
+    total_btc: totalinbtc,
+  };
 
   await Portfolio.save(portfolio);
 
@@ -76,9 +80,8 @@ async function start() {
   // 4. Calculate buy / sell price
   let sellPos = await Position.get(market).catch(console.error);
 
-
   let buyPrice = currPrice - currPrice * _stepDistance;
-  let buyAmount = totalBalanceBTC * _usedEquity / currPrice;
+  let buyAmount = (totalBalanceBTC * _usedEquity) / currPrice;
   let buyAmountRnd = +buyAmount.toFixed(3);
   let sellPrice = -1;
   let sellAmount = -1;
@@ -96,113 +99,129 @@ async function start() {
     buyAmountRnd = _minEquity;
   }
 
-  let buyPos: IPosition = {market: market, price: buyPrice, size: buyAmountRnd};
+  let buyPos: IPosition = {
+    market: market,
+    price: buyPrice,
+    size: buyAmountRnd,
+  };
 
   // If there is no sellPosition from the database place a market order
   if (sellPos === undefined) {
     try {
-      await client.placeOrder({
+      let resp = await client.placeOrder({
         market,
-        side: 'buy',
+        side: "buy",
         price: null,
-        type: 'market',
-        size: buyAmountRnd
+        type: "market",
+        size: buyAmountRnd,
       });
 
-      console.log(`[MARKET ORDER] BUY order: ${buyAmountRnd} ETH for ${currAsk}`);
-      await Position.set(market, currAsk, buyAmountRnd);
-      start();
+      if (resp.success) {
+        console.log(
+          `[MARKET ORDER] BUY order: ${buyAmountRnd} ETH for ${currAsk}`
+        );
+        await Position.set(market, currAsk, buyAmountRnd);
+        start();
+      } else {
+        console.warn(`[ALERT] Market order not successful ${resp.error}`);
+      }
     } catch (err) {
-      console.warn(`[ALERT] Market order not successful ${err}`)
+      console.warn(`[ALERT] Market order not successful ${err}`);
     }
-  }else {
-
+  } else {
     // 5. Place limit buy order
     try {
-      await client.placeOrder({
-        market,
-        side: 'buy',
-        price: buyPrice,
-        type: 'limit',
-        size: buyAmountRnd
-      });
-    //  console.log(`[LIMIT ORDER] BUY order placed: ${buyAmountRnd} for ${buyPrice}`);
+      let resp = await client
+        .placeOrder({
+          market,
+          side: "buy",
+          price: buyPrice,
+          type: "limit",
+          size: buyAmountRnd,
+        })
+        .catch(console.error);
 
+      if (!resp.success) {
+        console.warn(`[ALERT] Limit buy order not successful ${resp.error}`);
+      }
     } catch (err) {
-      console.warn(`[ALERT] Limit buy order not successful ${err}`)
+      console.warn(`[ALERT] Limit buy order not successful ${err}`);
     }
-
-
 
     // 6. Place limit sell order
     if (sellPrice > 0 && sellAmount > 0) {
       try {
-        await client.placeOrder({
+        let resp = await client.placeOrder({
           market,
-          side: 'sell',
+          side: "sell",
           price: sellPrice,
-          type: 'limit',
-          size: sellAmount
+          type: "limit",
+          size: sellAmount,
         });
 
-    //    console.log(`[LIMIT ORDER] SELL order placed: ${sellAmount} for ${sellPrice}`);
-      } catch (err) {
-        console.warn(`[ALERT] Limit sell order not successful ${err}`)
-      }
+        if (!resp.success) {
+          console.warn(`[ALERT] Limit sell order not successful ${resp.error}`);
+        }
 
+        //    console.log(`[LIMIT ORDER] SELL order placed: ${sellAmount} for ${sellPrice}`);
+      } catch (err) {
+        console.warn(`[ALERT] Limit sell order not successful ${err}`);
+      }
     }
   }
-}  
+}
 
 async function connect() {
-
   // Turn debugging off
-  DefaultLogger.silly = () => { };
-  DefaultLogger.debug = () => { };
-  DefaultLogger.notice = () => { };
-  DefaultLogger.info = () => { };
+  DefaultLogger.silly = () => {};
+  DefaultLogger.debug = () => {};
+  DefaultLogger.notice = () => {};
+  DefaultLogger.info = () => {};
 
   // Prepare a ws connection (connection init is automatic once ws client is instanced)
-  const ws = new WebsocketClient({ key: key, secret: secret, subAccountName: subAccountName },
-    DefaultLogger);
+  const ws = new WebsocketClient(
+    { key: key, secret: secret, subAccountName: subAccountName },
+    DefaultLogger
+  );
 
   // append event listeners
-  ws.on('update', msg => handleResponse(msg));
-  ws.on('error', msg => console.log('err: ', msg));
+  ws.on("update", (msg) => handleResponse(msg));
+  ws.on("error", (msg) => console.log("err: ", msg));
 
   // Subscribe to to topics
   // ws.subscribe(['fills', 'orders']);
-  ws.subscribe(['orders']);
+  ws.subscribe(["orders"]);
 
   await Database.open();
   await start();
-
 }
 
 async function handleResponse(msg: any) {
-
-
   if (msg.data != undefined) {
     // console.log('Message: ', msg);
-    if (msg.channel === 'orders'){
-
-      if (msg.data.status === 'closed' && msg.data.size === msg.data.filledSize){
-
-        if (msg.data.side === 'buy') {
-          if (msg.data.type === 'limit'){
+    if (msg.channel === "orders") {
+      if (
+        msg.data.status === "closed" &&
+        msg.data.size === msg.data.filledSize
+      ) {
+        if (msg.data.side === "buy") {
+          if (msg.data.type === "limit") {
             await Position.set(msg.data.market, msg.data.price, msg.data.size);
-            console.log(`[ORDER FILLED] ${msg.data.market}: ${msg.data.side} of ${msg.data.size} ETH for ${msg.data.price} BTC filled`)
+            console.log(
+              `[ORDER FILLED] ${msg.data.market}: ${msg.data.side} of ${msg.data.size} ETH for ${msg.data.price} BTC filled`
+            );
             start();
           }
-        } else if (msg.data.side === 'sell') {
+        } else if (msg.data.side === "sell") {
           await Position.deleteLast();
-          console.log(`[ORDER FILLED] ${msg.data.market}: ${msg.data.side} of ${msg.data.size} ETH for ${msg.data.price} BTC filled`)
+          console.log(
+            `[ORDER FILLED] ${msg.data.market}: ${msg.data.side} of ${msg.data.size} ETH for ${msg.data.price} BTC filled`
+          );
           start();
         }
       }
     }
   }
-
 }
 
 connect();
